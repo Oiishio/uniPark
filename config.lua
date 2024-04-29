e6f09fbb34ff372856b23f146f87bca8ade9eb07
Majamis69 <145318941+Majamis69@users.noreply.github.com>
2024-04-29 16:32:10 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,4 +2,4 @@ Config = {}
 
 Config.Price = 50
 Config.Jobs = {"police"}
-Config.Notification = 'ox_lib' -- Use 'ox_lib' if you want use ox lib notification. If you want use okokNotify then use 'okok'. If you want use your own type 'custom' and make export on client/functions.lua.
+Config.Notification = 'ox_lib' -- Use 'ox_lib' if you want use ox lib notification. If you want use okokNotify then use 'okok'. If you want use your own type 'custom' and make export on client/functions.lua or server/server.lua.
