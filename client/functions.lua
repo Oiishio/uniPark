b6055765352715239ebeb1f4afa3134a0a45e5fa
Majamis69 <145318941+Majamis69@users.noreply.github.com>
2024-04-29 16:30:18 +0300

    v1.1

--- client/functions.lua
+++ client/functions.lua
@@ -1,32 +1,39 @@
-RegisterNetEvent("uniPark:Success")
-AddEventHandler("uniPark:Success", function(cPlate)  
+lib.locale()
+
+Oi = {}
+local okok = exports['okokNotify']
+
+function Oi.Notify(title, desc, type)
+    if Config.Notification == 'ox_lib' then
     lib.notify({
-        title = 'uniPark',
-        description = 'Sekmingai nusipirkote bilietuka '..cPlate,
-        type = 'success'
+        title = title,
+        description =  desc,
+        type = type
     })
+elseif Config.Notification == 'okok' then
+    okok:Alert(title, desc, 5000, type)
+elseif Config.Notification == 'custom' then
+    -- Your code
+else
+    print('Notification system not found please check config.lua ---> Config.Notification and make sure that notification system is correct')
+    return
+    end
+end
+
+
+RegisterNetEvent("uniPark:Success")
+AddEventHandler("uniPark:Success", function(cPlate)
+    Oi.Notify(locale("title"), locale("bought_suc"):format(cPlate), 'success')
 end)
 RegisterNetEvent("uniPark:validTicket")
-AddEventHandler("uniPark:validTicket", function(cPlate)  
-    lib.notify({
-        title = 'uniPark',
-        description = 'Rastas bilietukas su numeriais '..cPlate,
-        type = 'success'
-    })
+AddEventHandler("uniPark:validTicket", function(cPlate)
+    Oi.Notify(locale("title"), locale("found_ticket"):format(cPlate), 'success')
 end)
 RegisterNetEvent("uniPark:expiredTicket")
-AddEventHandler("uniPark:expiredTicket", function(cPlate)  
-    lib.notify({
-        title = 'uniPark',
-        description = cPlate..' Bilietukas negalioja',
-        type = 'error'
-    })
+AddEventHandler("uniPark:expiredTicket", function(cPlate)
+    Oi.Notify(locale("title"), locale("expired"):format(cPlate), 'error')
 end)
 RegisterNetEvent("uniPark:invalidTicket")
-AddEventHandler("uniPark:invalidTicket", function(cPlate)  
-    lib.notify({
-        title = 'uniPark',
-        description = 'Bilietukas neegzistuoja su šiais numeriais '..cPlate,
-        type = 'error'
-    })
+AddEventHandler("uniPark:invalidTicket", function(cPlate)
+    Oi.Notify(locale("title"), locale("invalid"):format(cPlate), 'error')
 end)
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,7 @@
 -- Generated automaticly by RB Generator.
 fx_version('cerulean')
 games({ 'gta5' })
+author 'Oiishio'
 lua54 'yes'
 
 shared_script('@ox_lib/init.lua', 'config.lua');
@@ -11,6 +12,10 @@ server_scripts({
     '@oxmysql/lib/MySQL.lua',
     'config.lua',
     'server/server.lua',
+});
+
+files({
+    'locales/*.json'
 });
 
 client_scripts({
